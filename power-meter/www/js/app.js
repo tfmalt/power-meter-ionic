f6d77d9770f6b433678a3a7748822763fa7d5b47
Thomas Malt <thomas.malt@pearson.com>
2014-11-01 13:35:49 +0100

    Syncing latest changes.

--- power-meter/www/js/app.js
+++ power-meter/www/js/app.js
@@ -1,9 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-//
+/**
+ * App file for the
+ * @type {{}}
+ */
 
 var meter = {};
 meter.initialize = function() {
@@ -15,9 +13,9 @@ meter.initialize = function() {
         title: "Power Usage Now",
         label: "Watts"
     });
-}
+};
 
-var app = angular.module('power', ['ionic'])
+var app = angular.module('power', ['ionic']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
